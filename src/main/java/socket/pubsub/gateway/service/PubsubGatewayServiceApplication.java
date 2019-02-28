a7c744edbe1b40a7769aeb3c454380b61fec85a7
Qunfei Wu <qunfei.wu@tipico.com>
2019-02-28 23:50:18 +0100

    init project with spring boot

---
+++ src/main/java/socket/pubsub/gateway/service/PubsubGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package socket.pubsub.gateway.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubsubGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PubsubGatewayServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/socket/pubsub/gateway/service/PubsubGatewayServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package socket.pubsub.gateway.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PubsubGatewayServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
